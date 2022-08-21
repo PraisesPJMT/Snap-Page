2968223a986df0488b1dc0ea0f16df65be44be1c
PraisesPJMT <praisesmusa@outlook.com>
2022-08-22 00:10:58 +0100

    Implement desktop menu functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-// const mobileWindow = window.matchMedia('(max-width: 377px)');
-
 const mobileMenuOpen = document.querySelector('.menu-icon');
 const mobileMenuClose = document.querySelector('.close-icon');
 const featureTab = document.querySelector('.feature');
@@ -12,37 +10,97 @@ const dropDown = (input) => {
     } else {
         navBar = input;
     }
-    const nav = navBar.parentElement.querySelector('ul');
+    const nav = navBar.parentElement.querySelector('.sub-menu');
     const arrowUp = navBar.querySelector('.arrow-up');
     const arrowDown = navBar.querySelector('.arrow-down');
-    if (nav.style.display === 'none') {
-        nav.style.display = 'flex';
+    if (!nav.classList.contains('show')) {
+        nav.classList.remove('hide')
+        nav.classList.add('show');
         arrowDown.style.display = 'none';
         arrowUp.style.display = 'inline';
     } else {
-        nav.style.display = 'none'
+        nav.classList.remove('show');
+        nav.classList.add('hide')
         arrowDown.style.display = 'inline';
         arrowUp.style.display = 'none';
     }
 }
 
-if (window.innerWidth <= 414) {
-    mobileMenuOpen.addEventListener('click', () => {
-        const nav = document.querySelector('nav');
-        nav.style.display = 'block';
-    });
-
-    mobileMenuClose.addEventListener('click', () => {
-        const nav = document.querySelector('nav');
-        nav.style.display = 'none';
-    });
-
+if (window.innerWidth <= 769) {
+    const subNavs = document.querySelectorAll('.sub-menu');
+    subNavs.forEach((subNav) => {
+        subNav.classList.add('hide');
+    })
     featureTab.addEventListener('click', (event) => {
         dropDown(event.target);
     })
 
     companyTab.addEventListener('click', (event) => {
-        event.preventDefault();
         dropDown(event.target);
     })
+} else if (window.innerWidth > 769) {
+    featureTab.removeEventListener('click', () => {
+        const nav = document.querySelector('nav');
+        const subNavs = nav.querySelectorAll('.sub-menu');
+        nav.style.display = 'none';
+        subNavs.forEach((subNav) => {
+            subNav.classList.remove('hide');
+            subNav.classList.remove('show');
+        })
+    })
+
+    companyTab.removeEventListener('click', () => {
+        const nav = document.querySelector('nav');
+        const subNavs = nav.querySelectorAll('.sub-menu');
+        nav.style.display = 'none';
+        subNavs.forEach((subNav) => {
+            subNav.classList.remove('hide');
+            subNav.classList.remove('show');
+        })
+    })
 }
+
+window.addEventListener('resize', ()=> {
+    if (window.innerWidth <= 769) {
+        const nav = document.querySelector('nav');
+        nav.style.display = 'none';
+        const subNavs = nav.querySelectorAll('.sub-menu');
+        subNavs.forEach((subNav) => {
+            subNav.classList.add('hide');
+        })
+
+        featureTab.addEventListener('click', (event) => {
+            dropDown(event.target);
+        })
+
+        companyTab.addEventListener('click', (event) => {
+            dropDown(event.target);
+        })
+    } else  {
+        const nav = document.querySelector('nav');
+        nav.style.display = 'flex';
+        nav.querySelectorAll('.features-nav').forEach((subNav) => {
+            subNav.classList.remove('show');
+            subNav.classList.remove('hide');
+        })
+    }
+})
+
+mobileMenuOpen.addEventListener('click', () => {
+    const nav = document.querySelector('nav');
+    const subNavs = nav.querySelectorAll('.sub-menu');
+    nav.style.display = 'block';
+    subNavs.forEach((subNav) => {
+        subNav.classList.add('hide');
+    })
+});
+
+mobileMenuClose.addEventListener('click', () => {
+    const nav = document.querySelector('nav');
+    const subNavs = nav.querySelectorAll('.sub-menu');
+    nav.style.display = 'none';
+    subNavs.forEach((subNav) => {
+        subNav.classList.remove('hide');
+        subNav.classList.remove('show');
+    })
+});
